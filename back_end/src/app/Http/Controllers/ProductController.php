cc2db95c5ccb55d9292ab5cce81c49344547a9ef
Jake Lehner <jakelehner@gmail.com>
2019-11-24 18:58:24 -0500

    [API] Switching product API call to use product service

--- back_end/src/app/Http/Controllers/ProductController.php
+++ back_end/src/app/Http/Controllers/ProductController.php
@@ -7,21 +7,34 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use App\Http\Resources\ProductResource;
+use App\Services\ProductService\ProductService;
 
 class ProductController extends Controller
 {
     /**
+     * @var ProductService
+     */
+    private $productService;
+
+    /**
      * Create a new controller instance.
      *
+     * @var ProductService $productService
      * @return void
      */
-    public function __construct()
+    public function __construct(ProductService $productService)
     {
-        //
+        $this->productService = $productService;
     }
 
+    /**
+     * Returns all products
+     *
+     * @param Request $request
+     * @return void
+     */
     public function getProducts(Request $request)
     {
-        return ProductResource::collection(Product::all());
+        return ProductResource::collection($this->productService->fetchProducts());
     }
 }
